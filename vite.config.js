65b0b6700b1f9dbb06df7e82f40bd0c73ab88e5d
Reena B <reenaboopathy0474@gmail.com>
2026-08-20 12:42:37 +0530

    Fix Netlify asset paths

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  
-  base: '/Goat-Management-System/',
-  
+  base: '/',
   server: {
     port: 5173,
   },
